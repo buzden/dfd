793741ad0ddcbc3f25f146ef4d7226efba52a1a8
Denis Buzdalov <public@buzden.ru>
2019-01-31 14:49:39 +0300

    DFD implementation classes were put out of the DFD's companion object.

--- src/main/scala/ru/buzden/probability/dfd/DiscreteFiniteDistribution.scala
+++ src/main/scala/ru/buzden/probability/dfd/DiscreteFiniteDistribution.scala
@@ -30,37 +30,37 @@ sealed trait DiscreteFiniteDistribution[A, P] {
   private[dfd] def pure[F[_]](implicit A: Applicative[F]): F[DiscreteFiniteDistribution[A, P]] = A.pure(this)
 }
 
+// --- Discrete finite distributions implementations ---
+
+private final class MapDFD[A, P: Probability](pmfRaw: => Map[A, P])
+  extends DiscreteFiniteDistribution[A, P]  {
+  override lazy val pmf: Map[A, P] = pmfRaw `filter` { case (_, p) => p =!= zero } `withDefaultValue` zero
+  override lazy val support: Set[A] = pmf.keySet
+}
+
+private object MapDFD {
+  def apply[A, P: Probability](pmfRaw: Map[A, P]): DiscreteFiniteDistribution[A, P] = new MapDFD(pmfRaw)
+  def unapply[A, P](mDfd: MapDFD[A, P]): Option[Map[A, P]] = Some(mDfd.pmf)
+}
+
+private final class FunctionDFD[A, P: Probability](pmfRaw: A => P, supportRaw: => Set[A])
+  extends DiscreteFiniteDistribution[A, P] {
+  override lazy val pmf: A => P = a => if (support(a)) pmfRaw(a) else zero
+  override lazy val support: Set[A] = supportRaw `filter` { pmf(_) =!= zero }
+}
+
+private object FunctionDFD {
+  def apply[A, P: Probability](pmfRaw: A => P, supportRaw: Set[A]): DiscreteFiniteDistribution[A, P] =
+    new FunctionDFD[A, P](pmfRaw, supportRaw)
+  def unapply[A, P](fDfd: FunctionDFD[A, P]): Option[(A => P, Set[A])] = Some((fDfd.pmf, fDfd.support))
+}
+
 object DiscreteFiniteDistribution {
   type Errorable[Container[_]] = ApplicativeError[Container, NonEmptyList[String]]
   private def check[E[_]: Errorable](failMsg: => String)(v: Boolean): E[Unit] =
     if (!v) NonEmptyList.one(failMsg).raiseError[E, Unit] else ().pure[E]
   private def checkEq[E[_]: Errorable, A: Eq](desc: => String, actual: A, expected: A): E[Unit] =
     check(s"$desc is $actual but must be equal to $expected") { actual === expected }
-
-  // --- Discrete finite distributions implementations ---
-
-  private final class MapDFD[A, P: Probability](pmfRaw: => Map[A, P])
-      extends DiscreteFiniteDistribution[A, P]  {
-    override lazy val pmf: Map[A, P] = pmfRaw `filter` { case (_, p) => p =!= zero } `withDefaultValue` zero
-    override lazy val support: Set[A] = pmf.keySet
-  }
-
-  private object MapDFD {
-    def apply[A, P: Probability](pmfRaw: Map[A, P]): DiscreteFiniteDistribution[A, P] = new MapDFD(pmfRaw)
-    def unapply[A, P](mDfd: MapDFD[A, P]): Option[Map[A, P]] = Some(mDfd.pmf)
-  }
-
-  private final class FunctionDFD[A, P: Probability](pmfRaw: A => P, supportRaw: => Set[A])
-      extends DiscreteFiniteDistribution[A, P] {
-    override lazy val pmf: A => P = a => if (support(a)) pmfRaw(a) else zero
-    override lazy val support: Set[A] = supportRaw `filter` { pmf(_) =!= zero }
-  }
-
-  private object FunctionDFD {
-    def apply[A, P: Probability](pmfRaw: A => P, supportRaw: Set[A]): DiscreteFiniteDistribution[A, P] =
-      new FunctionDFD[A, P](pmfRaw, supportRaw)
-    def unapply[A, P](fDfd: FunctionDFD[A, P]): Option[(A => P, Set[A])] = Some((fDfd.pmf, fDfd.support))
-  }
 
   // --- Discrete finite distribution creation variants ---
 
